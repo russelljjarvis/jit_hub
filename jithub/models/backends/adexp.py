f73a02b0a1a889400d48938ee2eb4f0b537a2a47
Russell Jarvis <rjjarvis@asu.edu>
2021-01-11 12:49:39 +1100

    circle squash

--- jithub/models/backends/adexp.py
+++ jithub/models/backends/adexp.py
@@ -54,6 +54,142 @@ def evaluate_vm(time_trace,
 	  vm.append(v)
 	  i+=1
   return vm,spk_cnt
+class JIT_ADEXPBackend(Backend,RunnableModel):
+
+	name = 'ADEXP'
+	def __init__(self, attrs=None):
+		self.vM = None
+		self._attrs = attrs
+		BAE1 = {}
+		BAE1['cm']=0.281
+		BAE1['v_spike']=-40.0
+		BAE1['v_reset']=-70.6
+		BAE1['v_rest']=-70.6
+		BAE1['tau_m']=9.3667
+		BAE1['a']=4.0
+		BAE1['b']=0.0805
+
+		BAE1['delta_T']=2.0
+		BAE1['tau_w']=144.0
+		BAE1['v_thresh']=-50.4
+		BAE1['spike_delta']=30
+		self.default_attrs = BAE1
+
+		if type(attrs) is not type(None):
+			self._attrs = attrs
+		if self._attrs is None:
+			self._attrs = self.default_attrs
+		super().__init__(name='ADEXP')
+		super().init_backend(attrs=self.attrs,name='ADEXP')
+
+	def as_sciunit_model(self):
+		super().__init__()
+		return self
+
+
+	def set_stop_time(self, stop_time = 650*pq.ms):
+		"""Sets the simulation duration
+		stopTimeMs: duration in milliseconds
+		"""
+		self.tstop = float(stop_time.rescale(pq.ms))
+
+
+	def simulate(self, attrs={}, T=50,dt=0.25,integration_time=30, I_ext={},spike_delta=50):
+		spike_delta = spike_delta
+		N = 1
+		w = 1
+		dt         = dt
+		time_trace = np.arange(0,T+dt,dt)
+		len_time_trace = len(time_trace)
+		spike_raster = np.zeros((1, len_time_trace))
+		v_rest =  attrs['v_rest']
+		v = v_rest
+		v_reset = attrs['v_reset']
+		tau_m = attrs['tau_m']
+		delta_T = attrs['delta_T']
+		spike_delta = attrs['spike_delta']
+
+		a = attrs['a']
+		b = attrs['b']
+		v_thresh = attrs['v_thresh']
+		cm = attrs['cm']
+		tau_w = attrs['tau_w']
+		amp = I_ext['pA']
+		start = I_ext['start']
+		stop = I_ext['stop']
+
+		vm,n_spikes = evaluate_vm(time_trace,dt,T,v,w,b,a,
+						  spike_delta,spike_raster,v_reset,v_rest,
+						  tau_m,tau_w,v_thresh,delta_T,cm,amp,start,stop)
+		return vm,n_spikes
+
+
+	def get_spike_count(self):
+		return self.n_spikes
+
+
+	@property
+	def attrs(self):
+		return self._attrs
+
+	@attrs.setter
+	def attrs(self,attrs):
+		self.default_attrs.update(attrs)
+		attrs = self.default_attrs
+		self._attrs = attrs
+		if hasattr(self,'model'):
+			if not hasattr(self.model,'attrs'):
+				self.model.attrs = {}
+				self.model.attrs.update(attrs)
+
+
+	def get_membrane_potential(self):
+		"""Must return a neo.core.AnalogSignal.
+		"""
+		return self.vM
+	def set_stop_time(self, stop_time = 650*pq.ms):
+		"""Sets the simulation duration
+		stopTimeMs: duration in milliseconds
+		"""
+		self.tstop = float(stop_time.rescale(pq.ms))
+	def inject_square_current(self,amplitude=100*pq.pA, delay=10*pq.ms, duration=500*pq.ms):#, section = None, debug=False):
+		"""Inputs: current : a dictionary with exactly three items, whose keys are: 'amplitude', 'delay', 'duration'
+		Example: current = {'amplitude':float*pq.pA, 'delay':float*pq.ms, 'duration':float*pq.ms}}
+		where \'pq\' is a physical unit representation, implemented by casting float values to the quanitities \'type\'.
+		Description: A parameterized means of applying current injection into defined
+		Currently only single section neuronal models are supported, the neurite section is understood to be simply the soma.
+		"""
+		#self.set_attrs(self.attrs)
+		#temp_attrs =  self._attrs
+		amplitude = float(amplitude.magnitude)
+		duration = float(duration)
+		delay = float(delay)
+		tMax = delay + duration# + 200.0
+
+		self.set_stop_time(stop_time = tMax*pq.ms)
+		tMax = float(self.tstop)
+
+		stim = {'start':delay,'stop':duration+delay,'pA':amplitude}
+
+		vm,n_spikes = self.simulate(
+			attrs=self.attrs,\
+			T=tMax,\
+			dt=0.25,\
+			I_ext=stim)
+		vM = AnalogSignal(vm,
+							  units = voltage_units,
+							  sampling_period = 0.25*pq.ms)
+
+		self.vM = vM
+		self.n_spikes = n_spikes
+
+		return self.vM
+	def _backend_run(self):
+		results = {}
+		results['vm'] = self.vM.magnitude
+		results['t'] = self.vM.times
+		results['run_number'] = results.get('run_number',0) + 1
+		return results
 '''
 Too hard
 from numba import vectorize
@@ -118,141 +254,3 @@ def eval_model_collection(list_of_param_dicts):
 					delta_T,cm,amp,start,stop)
 	return result
 '''
-class JIT_ADEXPBackend(Backend,RunnableModel):
-
-	name = 'ADEXP'
-	#def init_backend(self):
-	#	super().init_backend()
-
-	def __init__(self, attrs=None):
-		super().__init__()
-		super().init_backend(attrs=self.attrs)
-		self.vM = None
-		self._attrs = attrs
-		BAE1 = {}
-		BAE1['cm']=0.281
-		BAE1['v_spike']=-40.0
-		BAE1['v_reset']=-70.6
-		BAE1['v_rest']=-70.6
-		BAE1['tau_m']=9.3667
-		BAE1['a']=4.0
-		BAE1['b']=0.0805
-
-		BAE1['delta_T']=2.0
-		BAE1['tau_w']=144.0
-		BAE1['v_thresh']=-50.4
-		BAE1['spike_delta']=30
-		self.default_attrs = BAE1
-
-		if type(attrs) is not type(None):
-			self._attrs = attrs
-		if self._attrs is None:
-			self._attrs = self.default_attrs
-
-	def as_sciunit_model(self):
-		super().__init__()
-		return self
-
-
-	def set_stop_time(self, stop_time = 650*pq.ms):
-		"""Sets the simulation duration
-		stopTimeMs: duration in milliseconds
-		"""
-		self.tstop = float(stop_time.rescale(pq.ms))
-
-
-	def simulate(self, attrs={}, T=50,dt=0.25,integration_time=30, I_ext={},spike_delta=50):
-		spike_delta = spike_delta
-		N = 1
-		w = 1
-		dt         = dt
-		time_trace = np.arange(0,T+dt,dt)
-		len_time_trace = len(time_trace)
-		spike_raster = np.zeros((1, len_time_trace))
-		v_rest =  attrs['v_rest']
-		v = v_rest
-		v_reset = attrs['v_reset']
-		tau_m = attrs['tau_m']
-		delta_T = attrs['delta_T']
-		spike_delta = attrs['spike_delta']
-
-		a = attrs['a']
-		b = attrs['b']
-		v_thresh = attrs['v_thresh']
-		cm = attrs['cm']
-		tau_w = attrs['tau_w']
-		amp = I_ext['pA']
-		start = I_ext['start']
-		stop = I_ext['stop']
-
-		vm,n_spikes = evaluate_vm(time_trace,dt,T,v,w,b,a,
-						  spike_delta,spike_raster,v_reset,v_rest,
-						  tau_m,tau_w,v_thresh,delta_T,cm,amp,start,stop)
-		return vm,n_spikes
-
-
-	def get_spike_count(self):
-		return self.n_spikes
-
-
-	@property
-	def attrs(self):
-		return self._attrs
-
-	@attrs.setter
-	def attrs(self,attrs):
-		self.default_attrs.update(attrs)
-		attrs = self.default_attrs
-		self._attrs = attrs
-		if not hasattr(self.model,'attrs'):
-			self.model.attrs = {}
-			self.model.attrs.update(attrs)
-
-
-	def get_membrane_potential(self):
-		"""Must return a neo.core.AnalogSignal.
-		"""
-		return self.vM
-	def set_stop_time(self, stop_time = 650*pq.ms):
-		"""Sets the simulation duration
-		stopTimeMs: duration in milliseconds
-		"""
-		self.tstop = float(stop_time.rescale(pq.ms))
-	def inject_square_current(self,amplitude=100*pq.pA, delay=10*pq.ms, duration=500*pq.ms):#, section = None, debug=False):
-		"""Inputs: current : a dictionary with exactly three items, whose keys are: 'amplitude', 'delay', 'duration'
-		Example: current = {'amplitude':float*pq.pA, 'delay':float*pq.ms, 'duration':float*pq.ms}}
-		where \'pq\' is a physical unit representation, implemented by casting float values to the quanitities \'type\'.
-		Description: A parameterized means of applying current injection into defined
-		Currently only single section neuronal models are supported, the neurite section is understood to be simply the soma.
-		"""
-		#self.set_attrs(self.attrs)
-		#temp_attrs =  self._attrs
-		amplitude = float(amplitude.magnitude)
-		duration = float(duration)
-		delay = float(delay)
-		tMax = delay + duration# + 200.0
-
-		self.set_stop_time(stop_time = tMax*pq.ms)
-		tMax = float(self.tstop)
-
-		stim = {'start':delay,'stop':duration+delay,'pA':amplitude}
-
-		vm,n_spikes = self.simulate(
-			attrs=self.attrs,\
-			T=tMax,\
-			dt=0.25,\
-			I_ext=stim)
-		vM = AnalogSignal(vm,
-							  units = voltage_units,
-							  sampling_period = 0.25*pq.ms)
-
-		self.vM = vM
-		self.n_spikes = n_spikes
-
-		return self.vM
-	def _backend_run(self):
-		results = {}
-		results['vm'] = self.vM.magnitude
-		results['t'] = self.vM.times
-		results['run_number'] = results.get('run_number',0) + 1
-		return results
