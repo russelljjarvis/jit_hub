271dadf25211baff66da671bfc59c1c267b3e32c
Russell Jarvis <russelljarvis@protonmail.com>
2021-01-26 22:08:30 +1100

    more rigorous unit test

--- unittest/basic_run.py
+++ unittest/basic_run.py
@@ -2,6 +2,7 @@ import jithub
 from jithub.models import model_classes
 import quantities as pq
 cellmodels = ["IZHI","MAT","ADEXP"]
+import unittest
 for cellmodel in cellmodels:
     if cellmodel == "IZHI":
         model = model_classes.IzhiModel()
@@ -9,13 +10,13 @@ for cellmodel in cellmodels:
         model = model_classes.MATModel()
     if cellmodel == "ADEXP":
         model = model_classes.ADEXPModel()
-ALLEN_DELAY = 1000.0 * pq.ms
-ALLEN_DURATION = 2000.0 * pq.ms
-uc = {
-    "amplitude": 25*pq.pA,
-    "duration": ALLEN_DURATION,
-    "delay": ALLEN_DELAY,
-}
-model.inject_square_current(**uc)
-vm = model.get_membrane_potential()
-assert vm.times[-1] == ALLEN_DELAY + ALLEN_DURATION
+    ALLEN_DELAY = 1000.0 * pq.ms
+    ALLEN_DURATION = 2000.0 * pq.ms
+    uc = {
+        "amplitude": 25*pq.pA,
+        "duration": ALLEN_DURATION,
+        "delay": ALLEN_DELAY,
+    }
+    model.inject_square_current(**uc)
+    vm = model.get_membrane_potential()
+    assert vm.times[-1] == ALLEN_DELAY + ALLEN_DURATION
